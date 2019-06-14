2834c4df9ae97a70b18b3de2a1a7cbf7ebdb8d75
Shujinko53 <46525278+Shujinko53@users.noreply.github.com>
2019-06-14 23:52:00 +0300

    task 3

---
+++ js/setup.js
@@ -0,0 +1,55 @@
+'use strict';
+
+var WIZARD_NAMES = ['Дамблдор', 'Волдеморт', 'Доктор Стрендж', 'Гарри Поттер'];
+var WIZARD_COAT = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var WIZARD_EYES = ['black', 'red', 'blue', 'yellow', 'green'];
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
+
+var similarListElement = userDialog.querySelector('.setup-similar-list');
+
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+    .content
+    .querySelector('.setup-similar-item');
+
+
+var wizards = [
+  {
+    name: WIZARD_NAMES[0],
+    coatColor: WIZARD_COAT[0],
+    eyesColor: WIZARD_EYES[0]
+  },
+  {
+    name: WIZARD_NAMES[1],
+    coatColor: WIZARD_COAT[5],
+    eyesColor: WIZARD_EYES[2]
+  },
+  {
+    name: WIZARD_NAMES[2],
+    coatColor: WIZARD_COAT[2],
+    eyesColor: WIZARD_EYES[1]
+  },
+  {
+    name: WIZARD_NAMES[3],
+    coatColor: WIZARD_COAT[3],
+    eyesColor: WIZARD_EYES[4]
+  }
+];
+
+
+var renderWizard = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+
+  return wizardElement;
+};
+
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < wizards.length; i++) {
+  fragment.appendChild(renderWizard(wizards[i]));
+}
+similarListElement.appendChild(fragment);
+
+userDialog.querySelector('.setup-similar').classList.remove('hidden');
