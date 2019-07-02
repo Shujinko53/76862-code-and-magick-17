fb903e9ac9f5e50a16af8441b1737fb58d6330bf
Shujinko53 <46525278+Shujinko53@users.noreply.github.com>
2019-07-02 22:46:16 +0300

    Create backend.js

---
+++ js/backend.js
@@ -0,0 +1,18 @@
+'use strict';
+
+(function () {
+
+  var URL = 'https://js.dump.academy/code-and-magick/data';
+
+  backend.load = function (onLoad, onError) {
+
+
+  };
+
+  backend.save = function (data, onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+  };
+
+})();
