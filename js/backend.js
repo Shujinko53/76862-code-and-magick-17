cef7e6ee3bc516dee79cb6e8256e05a602cb579a
Shujinko53 <46525278+Shujinko53@users.noreply.github.com>
2019-07-06 15:53:40 +0300

    Update backend.js

--- js/backend.js
+++ js/backend.js
@@ -2,17 +2,19 @@
 
 (function () {
 
-  var URL = 'https://js.dump.academy/code-and-magick/data';
-
-  backend.load = function (onLoad, onError) {
-
-
-  };
-
-  backend.save = function (data, onLoad, onError) {
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
-
-  };
-
+  window.backend = {
+    
+    load: function (onLoad, onError) {
+      var xhr = new XMLHttpRequest();
+      var URL = 'https://js.dump.academy/code-and-magick/data'; 
+      
+    }
 })();
+  // 
+
+  //   backend.load = function (onLoad, onError) {
+  //   };
+
+  //   backend.save = function (data, onLoad, onError) {
+  //     var xhr = new XMLHttpRequest();
+  //   xhr.responseType = 'json'; }
