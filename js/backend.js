88b63680ffd6542b6b57d8b4d059ebe2308001e9
Shujinko53 <46525278+Shujinko53@users.noreply.github.com>
2019-07-06 19:11:35 +0300

    backend.load and save function

--- js/backend.js
+++ js/backend.js
@@ -1,20 +1,59 @@
 'use strict';
 
 (function () {
-
   window.backend = {
-    
+
     load: function (onLoad, onError) {
       var xhr = new XMLHttpRequest();
-      var URL = 'https://js.dump.academy/code-and-magick/data'; 
-      
+      var URL = 'https://js.dump.academy/code-and-magick/data';
+
+      xhr.responseType = 'json';
+
+      xhr.addEventListener('load', function () {
+        if (xhr.status === 200) {
+          onLoad(xhr.response);
+        } else {
+          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+        }
+      });
+
+      xhr.addEventListener('error', function () {
+        onError('Ошибка соединения');
+      });
+
+      xhr.addEventListener('timeout', function () {
+        onError('Запрос не успел выполниться' + xhr.timeout + 'мс');
+      });
+
+      xhr.open('GET', URL);
+
+      xhr.send();
+    },
+
+    save: function (data, onLoad, onError) {
+      var xhr = new XMLHttpRequest();
+      var URL = 'https://js.dump.academy/code-and-magick/data';
+
+      xhr.addEventListener('load', function () {
+        if (xhr.status === 200) {
+          data = xhr.response.slice(0, 4);
+          onLoad(data);
+        } else {
+          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+        }
+      });
+
+      xhr.addEventListener('error', function () {
+        onError('Ошибка соединения');
+      });
+
+      xhr.addEventListener('timeout', function () {
+        onError('Запрос не успел выполниться' + xhr.timeout + 'мс');
+      });
+
+      xhr.open('GET', URL);
+
     }
+
+  };
 })();
-  // 
-
-  //   backend.load = function (onLoad, onError) {
-  //   };
-
-  //   backend.save = function (data, onLoad, onError) {
-  //     var xhr = new XMLHttpRequest();
-  //   xhr.responseType = 'json'; }
