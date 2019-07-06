c4383c8823d3531df3197eb551027acc2b521b30
Shujinko53 <46525278+Shujinko53@users.noreply.github.com>
2019-07-06 19:11:52 +0300

    add filter wizards

--- js/setup.js
+++ js/setup.js
@@ -1,7 +1,7 @@
 'use strict';
 
 (function () {
-  var WIZARD_NAMES = ['Дамблдор', 'Волдеморт', 'Доктор Стрендж', 'Гарри Поттер'];
+  // var WIZARD_NAMES = ['Дамблдор', 'Волдеморт', 'Доктор Стрендж', 'Гарри Поттер'];
   var WIZARD_COAT = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
   var WIZARD_EYES = ['black', 'red', 'blue', 'yellow', 'green'];
   var WIZARD_FIRE = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
@@ -14,28 +14,28 @@
 
   userDialog.classList.remove('hidden');
 
-  var wizards = [
-    {
-      name: WIZARD_NAMES[0],
-      coatColor: WIZARD_COAT[0],
-      eyesColor: WIZARD_EYES[0]
-    },
-    {
-      name: WIZARD_NAMES[1],
-      coatColor: WIZARD_COAT[5],
-      eyesColor: WIZARD_EYES[2]
-    },
-    {
-      name: WIZARD_NAMES[2],
-      coatColor: WIZARD_COAT[2],
-      eyesColor: WIZARD_EYES[1]
-    },
-    {
-      name: WIZARD_NAMES[3],
-      coatColor: WIZARD_COAT[3],
-      eyesColor: WIZARD_EYES[4]
-    }
-  ];
+  // var wizards = [
+  //   {
+  //     name: WIZARD_NAMES[0],
+  //     coatColor: WIZARD_COAT[0],
+  //     eyesColor: WIZARD_EYES[0]
+  //   },
+  //   {
+  //     name: WIZARD_NAMES[1],
+  //     coatColor: WIZARD_COAT[5],
+  //     eyesColor: WIZARD_EYES[2]
+  //   },
+  //   {
+  //     name: WIZARD_NAMES[2],
+  //     coatColor: WIZARD_COAT[2],
+  //     eyesColor: WIZARD_EYES[1]
+  //   },
+  //   {
+  //     name: WIZARD_NAMES[3],
+  //     coatColor: WIZARD_COAT[3],
+  //     eyesColor: WIZARD_EYES[4]
+  //   }
+  // ];
 
 
   var renderWizard = function (wizard) {
@@ -48,14 +48,15 @@
     return wizardElement;
   };
 
-  var fragment = document.createDocumentFragment();
-  for (var i = 0; i < wizards.length; i++) {
-    fragment.appendChild(renderWizard(wizards[i]));
-  }
-  similarListElement.appendChild(fragment);
+  window.backend.load(function (wizards) {
+    var fragment = document.createDocumentFragment();
+    for (var i = 0; i < 4; i++) {
+      fragment.appendChild(renderWizard(wizards[i]));
+    }
+    similarListElement.appendChild(fragment);
 
-  userDialog.querySelector('.setup-similar').classList.remove('hidden');
-
+    userDialog.querySelector('.setup-similar').classList.remove('hidden');
+});
 
   /* Проверка на валидность введенного имени персонажа -- */
 
@@ -82,7 +83,18 @@
   var fireball = userDialog.querySelector('.setup-fireball-wrap');
 
   wizardCoat.addEventListener('click', function () {
+    var filterValue = wizardCoat.style.fill;
     wizardCoat.style.fill = WIZARD_COAT[Math.floor(WIZARD_COAT.length * Math.random())];
+
+    var filterWizard = wizards.filter(function (item) {
+      if (item.colorCoat === filterValue) {
+        return item;
+      } else {
+        return false;
+      }
+    });
+    wizard.renderWizard(filterWizard);
+
   });
 
   wizardEyes.addEventListener('click', function () {
